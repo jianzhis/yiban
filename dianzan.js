e847d23e04248cf270f9987cc1c5f976fc3c2d44
jianzhicvs <934075733@qq.com>
2023-11-03 01:17:59 +0800

    代码优化：Update - 优化抓取文章代码 · 优化点赞代码

--- dianzan.js
+++ dianzan.js
@@ -1,34 +1,41 @@
-const request = require("request");
-const fs = require("fs");
+const rp = require("request-promise-native");
+const fs = require("fs").promises;
 require("dotenv").config();
-const yiban_user_token = process.env.YIBAN_USER_TOKEN;
-const yiban_user_id = process.env.YIBAN_USER_ID;
 
-let currentIndex = 0;
+const { YIBAN_USER_TOKEN, YIBAN_USER_SSID, YIBAN_USER_ID } = process.env;
+
+if (!YIBAN_USER_TOKEN || !YIBAN_USER_SSID || !YIBAN_USER_ID) {
+  throw new Error("缺少必要的环境变量。");
+}
+
 const file = "db.txt";
 
-function readCurrentIndex() {
+let currentIndex = 0;
+let requestCount = 0;
+
+async function readCurrentIndex() {
   try {
-    currentIndex = parseInt(fs.readFileSync(file, "utf8")) || 0;
-    console.log("Current Index:", currentIndex);
+    const data = await fs.readFile(file, "utf8");
+    return parseInt(data) || 0;
   } catch (error) {
     console.error("读取文件时发生错误", error);
+    return 0;
   }
 }
 
-function updateCurrentIndex() {
+async function updateCurrentIndex() {
   try {
-    fs.writeFileSync(file, currentIndex.toString());
+    await fs.writeFile(file, currentIndex.toString());
     console.log("写入成功");
   } catch (err) {
     console.error("写入文件时发生错误", err);
   }
 }
 
-function thumbPost(postId) {
+async function thumbPost(postId) {
   const options = {
     method: "POST",
-    url: "https://s.yiban.cn/api/post/thumb",
+    uri: "https://s.yiban.cn/api/post/thumb",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json, text/plain, */*",
@@ -45,44 +52,38 @@ function thumbPost(postId) {
         '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
       "sec-ch-ua-mobile": "?0",
       "sec-ch-ua-platform": '"Windows"',
-      Cookie: `MESSAGE_NEW_VERSION=1; preview_hidden=0; https_waf_cookie=ac1c8da0-060b-425a8b8071f6b9b7e75132390ee733c5f864; YB_SSID=21a05b01349f782468dd1d1384c882ea; timezone=-8; Hm_lvt_ed61a54fb63b75cc82ad5c1796518f16=1697769830,1698217713,1698759582,1698815816; yiban_user_token=${yiban_user_token}; Hm_lpvt_ed61a54fb63b75cc82ad5c1796518f16=1698818122`,
+      Cookie: `MESSAGE_NEW_VERSION=1; preview_hidden=0; https_waf_cookie=ac1c8da0-060b-425a8b8071f6b9b7e75132390ee733c5f864; YB_SSID=${YIBAN_USER_SSID}; timezone=-8; Hm_lvt_ed61a54fb63b75cc82ad5c1796518f16=1697769830,1698217713,1698759582,1698815816; yiban_user_token=${YIBAN_USER_TOKEN}; Hm_lpvt_ed61a54fb63b75cc82ad5c1796518f16=1698818122`,
       Host: "s.yiban.cn",
     },
-    body: JSON.stringify({
+    body: {
       action: "up",
       postId: postId,
-      userId: yiban_user_id,
-    }),
+      userId: YIBAN_USER_ID,
+    },
+    json: true,
   };
 
-  request(options, function (error, response, body) {
-    if (error) {
-      console.error("发送请求时发生错误", error);
-      return;
-    }
-
-    try {
-      const responseData = JSON.parse(body);
-      console.log("响应数据:", responseData);
-    } catch (parseError) {
-      console.error("解析响应数据时发生错误", parseError);
-    }
-  });
+  try {
+    const responseData = await rp(options);
+    console.log("响应数据:", responseData);
+  } catch (error) {
+    console.error("发送请求时发生错误", error);
+  }
 }
 
-function processItem(index, jsonData) {
-  if (index < jsonData.length) {
+async function processItem(index, jsonData) {
+  if (index < jsonData.length && requestCount < 30) {
     const currentItem = jsonData[index];
     const itemId = currentItem.id;
-    const numId = currentItem.num;
 
-    console.log("Current Item ID:", itemId, numId);
+    console.log("Current Item ID:", itemId);
 
-    thumbPost(itemId);
+    await thumbPost(itemId);
 
     currentIndex++;
+    requestCount++;
 
-    updateCurrentIndex();
+    await updateCurrentIndex();
 
     setTimeout(() => {
       processItem(index + 1, jsonData);
@@ -90,15 +91,14 @@ function processItem(index, jsonData) {
   }
 }
 
-readCurrentIndex();
+(async () => {
+  currentIndex = await readCurrentIndex();
 
-fs.readFile("data.json", "utf8", (err, data) => {
-  if (err) {
-    console.error("读取文件时发生错误", err);
-    return;
+  try {
+    const data = await fs.readFile("data.json", "utf8");
+    const jsonData = JSON.parse(data);
+    await processItem(currentIndex, jsonData);
+  } catch (error) {
+    console.error("发生错误", error);
   }
-
-  const jsonData = JSON.parse(data);
-
-  processItem(currentIndex, jsonData);
-});
+})();
